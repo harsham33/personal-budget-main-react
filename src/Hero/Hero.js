5ceb29203a64633fa928797b72dd2fe0fd282a1d
harsham33 <mulgeharsha03@gmail.com>
2024-09-29 19:20:41 -0400

    updated code

---
+++ src/Hero/Hero.js
@@ -0,0 +1,14 @@
+import {
+    Link
+  } from "react-router-dom";
+
+function Hero() {
+    return (
+        <header className="hero" role="banner">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+    </header>
+    );
+  }
+  
+  export default Hero;
